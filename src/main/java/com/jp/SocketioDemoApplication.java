f89c26701bba0a35bd4dc3923ad1b1eee2835317
guoyi8 <75337656@qq.com>
2018-05-16 00:24:20 +0800

    初次提交

---
+++ src/main/java/com/jp/SocketioDemoApplication.java
@@ -0,0 +1,12 @@
+package com.jp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketioDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketioDemoApplication.class, args);
+	}
+}
